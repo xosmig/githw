c22614f207c090768f60a43458c779f452bff315
Andrey Tonkikh <xosmig@gmail.com>
2017-03-14 01:48:00 +0300

    TODO added to remove command

--- src/main/kotlin/com/xosmig/githw/commands/RemoveCommand.kt
+++ src/main/kotlin/com/xosmig/githw/commands/RemoveCommand.kt
@@ -3,10 +3,13 @@ package com.xosmig.githw.commands
 import com.xosmig.githw.GIT_DIR_PATH
 import com.xosmig.githw.index.IndexEntry
 import java.io.IOException
+import java.nio.file.Files
 import java.nio.file.Path
 
 @Throws(IOException::class)
 fun remove(root: Path, file: Path) {
     val gitDir = root.resolve(GIT_DIR_PATH)
+    TODO("check that file is tracked")
+    Files.deleteIfExists(file)
     IndexEntry.RemoveFile(gitDir, root.relativize(file)).writeToDisk()
 }
